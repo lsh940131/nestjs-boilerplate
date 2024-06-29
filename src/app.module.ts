734648378205723c6e5e78e3eed19e52b2e2fd27
lsh <ls940131@naver.com>
2024-06-29 23:36:39 +0900

    INIT] auth & user APIs, prisma, swagger

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './domain/auth/auth.module';
+import { UserModule } from './domain/user/user.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionFilter } from './filter/exception.filter';
+import { ResponseInterceptor } from './interceptor/response.interceptor';
+
+@Module({
+	imports: [PrismaModule, AuthModule, UserModule],
+	controllers: [AppController],
+	providers: [
+		AppService,
+		{
+			provide: APP_FILTER,
+			useClass: AllExceptionFilter,
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: ResponseInterceptor,
+		},
+	],
+})
+export class AppModule {}
